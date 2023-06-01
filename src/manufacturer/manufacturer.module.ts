f4031fa6addb1342dfca0b148604b8e57153daaf
aminechaabini <amine.chaabini@insat.ucar.tn>
2023-06-01 16:07:19 +0100

    fix db problem

--- src/manufacturer/manufacturer.module.ts
+++ src/manufacturer/manufacturer.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ManufacturerController } from './manufacturer.controller';
 import { ManufacturerService } from './manufacturer.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Category} from "../category/entities/category.entity";
+import {Manufacturer} from "./entities/manufacturer.entity";
 
 @Module({
+  imports : [TypeOrmModule.forFeature([Manufacturer])],
   controllers: [ManufacturerController],
   providers: [ManufacturerService]
 })
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Category} from "../category/entities/category.entity";
+import {Product} from "./entities/product.entity";
 
 @Module({
+  imports : [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
   providers: [ProductService]
 })
